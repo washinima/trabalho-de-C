afc3de973885dbe9ceef8f3b05e1b734b5cd92f5
Unknown <brunofreitas5.25@hotmail.com>
2017-04-27 21:56:39 +0100

    Mais algumas coisas

--- Trabalho de C/Trabalho de C/Listas.c
+++ Trabalho de C/Trabalho de C/Listas.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
 #include "Main.h"
 
-int Inserir()
+
+/*
+Função que aloca memória para uma nova jogada
+*/
+JogadasPtr NovaJogada(PecaPtr peca, Coordenadas movimento)
+{
+	JogadasPtr nova = (JogadasPtr)malloc(sizeof(Jogadas));
+	nova->peca = peca;
+	nova->movimento = movimento;
+	nova->next = NULL;
+}
+
+
+/*
+Função que insere a jogada mais recente na lista de jogadas
+*/
+JogadasPtr Inserir(JogadasPtr head, Jogadas novo)
 {
 
 }
--- Trabalho de C/Trabalho de C/Main.h
+++ Trabalho de C/Trabalho de C/Main.h
@@ -7,18 +7,18 @@ typedef struct Coordenadas {
 }Coordenadas;
 
 
-
-
 typedef struct Peca {
-	char* tipo; //peão, rei, peão "melhorado"
+	int numPeca;				//1  2  3  4  5  6  7  8 / 9  10 11 12 13 14 15 16 <--- demonstração dos números de cada peça no tabuleiro
+	char* tipo;					//peão, rei, peão "melhorado"
 	Coordenadas posicao;
-};
+}Peca;
 
-
+typedef Peca *PecaPtr;
 
 
 typedef struct Jogadas {
-
+	PecaPtr peca;
+	Coordenadas movimento;
 	JogadasPtr next;
 }Jogadas;
 
